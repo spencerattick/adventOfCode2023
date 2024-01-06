8af594ab1cedb2a4a6b2153edaa0af069e33dd5c
Spencer Attick <spencer.attick@gmail.com>
2024-01-06 12:00:33 -0500

    solve Day5-1

--- Day5/Day5-1.js
+++ Day5/Day5-1.js
@@ -1,106 +1,67 @@
 const input = ``
 
-let seeds = []
-let seedToSoilMap = {}
-let soilToFertilizerMap = {}
-let fertilizerToWaterMap = {}
-let waterToLightMap = {}
-let lightToTemperatureMap = {}
-let temperatureToHumidityMap = {}
-let humidityToLocationMap = {}
+
+let seeds = {}
 
 const findLowestLocationPerSeed = seeds => {
-    let lowestLocation = Infinity
-    let lowestLocationSeed
-
-    for (let seed of seeds) {
-        const seedToSoilNum = seedToSoilMap[seed] || seed
-        const soilToFertilizerNum = soilToFertilizerMap[seedToSoilNum] || seedToSoilNum
-        const fertilizerToWaterNum = fertilizerToWaterMap[soilToFertilizerNum] || soilToFertilizerNum
-        const waterToLightNum = waterToLightMap[fertilizerToWaterNum] || fertilizerToWaterNum
-        const lightToTemperatureNum = lightToTemperatureMap[waterToLightNum] || waterToLightNum
-        const temperatureToHumidityNum = temperatureToHumidityMap[lightToTemperatureNum] || lightToTemperatureNum
-        const humidityToLocationNum = humidityToLocationMap[temperatureToHumidityNum] || temperatureToHumidityNum
-
-        if (humidityToLocationNum < lowestLocation) {
-            lowestLocation = humidityToLocationNum
-            lowestLocationSeed = seed
+    let lowest = Infinity
+    for (let arr of Object.values(seeds)) {
+        if (arr[arr.length - 1] < lowest) {
+            lowest = arr[arr.length - 1]
         }
     }
-    return lowestLocation
+    return lowest
 }
 
-const createMap = lineToMap => {
-    const mapObj = {}
-    for (let row of lineToMap) {
-        const splitRow = row.split(' ')
-        const destinationRangeStart = Number(splitRow[0])
-        const sourceRangeStart = Number(splitRow[1])
-        const rangeLength = Number(splitRow[2])
-        let destinationCounter = destinationRangeStart
-        let sourceCounter = sourceRangeStart
-        for (let i = 0; i < rangeLength; i++) {
-            mapObj[sourceCounter] = destinationCounter
-            destinationCounter++
-            sourceCounter++
-        }
+const parseSeedLine = seedsLine => {
+    let splitSeeds = seedsLine.split(':')
+    splitSeeds.shift()
+    splitSeeds = splitSeeds[0].split(' ')
+    splitSeeds.shift()
+    for (let seed of splitSeeds) {
+        seeds[seed] = [seed]
     }
-    return mapObj
+}
+
+const isValueIncludedInPreviousMapping = line => {
+    let mappedNum
+    for (let seed in seeds) {
+        const numToLookAt = seeds[seed][seeds[seed].length - 1]
+        for (let nums of line) {
+            const splitNums = nums.split(' ')
+            const numToStart = Number(splitNums[1])
+            const destStartNum = Number(splitNums[0])
+            const range = Number(splitNums[2])
+            if (numToLookAt <= numToStart + (range - 1) && numToLookAt >= numToStart) {
+                const calcNum = Number(numToLookAt) - numToStart
+                mappedNum = destStartNum + calcNum
+                break
+            } else {
+                mappedNum = numToLookAt
+            }
+        }
+        seeds[seed].push(mappedNum)
+    }
+}
+
+const parseNonSeedLine = line => {
+    const splitLine = line.split('\n')
+    const nameOfLine = splitLine[0]
+    splitLine.shift()
+    return splitLine
 }
 
 const parseData = input => {
-    const splitInput = input.split('\n\n')
-
+    const splitInput = input.split('\n\n')    
     for (let line of splitInput) {
-        const mapType = line.split(':')[0]
-        if (mapType === 'seeds') {
-            let splitLine = line.split(':')
-            splitLine.shift()
-            splitLine = splitLine[0].split(' ')
-            splitLine.shift()
-            seeds = splitLine
-        } else if (mapType === 'seed-to-soil map') {
-            line = line.split('\n')
-            line.shift()
-            seedToSoilMap = createMap(line)
-        } else if (mapType === 'soil-to-fertilizer map') {
-            line = line.split('\n')
-            line.shift()
-            soilToFertilizerMap = createMap(line)
-        } else if (mapType === 'fertilizer-to-water map') {
-            line = line.split('\n')
-            line.shift()
-            fertilizerToWaterMap = createMap(line)
-        } else if (mapType === 'water-to-light map') {
-            line = line.split('\n')
-            line.shift()
-            waterToLightMap = createMap(line)
-        } else if (mapType === 'light-to-temperature map') {
-            line = line.split('\n')
-            line.shift()
-            lightToTemperatureMap = createMap(line)
-        } else if (mapType === 'temperature-to-humidity map') {
-            line = line.split('\n')
-            line.shift()
-            temperatureToHumidityMap = createMap(line)
-        } else if (mapType === 'humidity-to-location map') {
-            line = line.split('\n')
-            line.shift()
-            humidityToLocationMap = createMap(line)
+        if (line.includes('seeds')) {
+            parseSeedLine(line)
+        } else {
+            const dataToLookAt = parseNonSeedLine(line)
+            isValueIncludedInPreviousMapping(dataToLookAt)
         }
-
     }
-
-    // console.log('SEEDS: ', seeds)
-    // console.log('SEEDTOSOIL: ', seedToSoilMap)
-    // console.log('SOILTOFERTILIZER: ', soilToFertilizerMap)
-    // console.log('FERTILIZERTOWATER: ', fertilizerToWaterMap)
-    // console.log('WATERTOLIGHT: ', waterToLightMap)
-    // console.log('LIGHTTOTEMP: ', lightToTemperatureMap)
-    // console.log('TEMPTOHUMIDITY: ', temperatureToHumidityMap)
-    // console.log('HUMIDITYTOLOCATION: ', humidityToLocationMap)
-
-    return findLowestLocationPerSeed(seeds)
+   return findLowestLocationPerSeed(seeds)
 
 }
 
